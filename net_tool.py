2b4ccfd57fbc21336b0bbb7e80618a020bf2690e
reevesrs24 <reevesrs24@gmail.com>
2018-07-10 22:31:01 -0500

    test ap

--- net_tool.py
+++ net_tool.py
@@ -1,10 +1,12 @@
 import sys
 import os
 from scapy.all import *
+import threading
+from multiprocessing import Process
 
 interface= "wlx00c0ca84aba2"
-ap_mac = "2c:30:33:65:bd:93"
-device_mac = "60:be:b5:f0:23:b1"
+ap_mac = "FC:52:8D:77:B8:A9"
+device_mac = "40:4e:36:d2:eb:b2"
 macAddresses = []
 probeReqs = []
 hidden_aps = []
@@ -34,14 +36,21 @@ def find_hidden_ap(pkt):
 		addr2 = pkt.getlayer(Dot11).addr2
 		if (addr2 in hidden_aps) and (addr2 not in aps):
 			name = pkt.getlayer(Dot11ProbeResp).info
-			print '[+]' + name + ' ' + addr2
+			print '[+] ' + name + ' ' + addr2
 			aps.append(addr2)
 	if pkt.haslayer(Dot11Beacon):
-		print pkt.summary()
 		if pkt.getlayer(Dot11).info == '':
 			addr2 = pkt.getlayer(Dot11).addr2
 			if addr2 not in hidden_aps:
 				hidden_aps.append(addr2)
+
+def sniff_ap_traffic(pkt):
+	if pkt.haslayer(Dot11):
+		layer = pkt.getlayer(Dot11)
+		if layer.addr1 == "00:c0:ca:84:ab:a2":
+			print "[+] " + layer.addr1
+
+
 
 def get_probe_requests():
     print "getting probe requests.."
@@ -62,9 +71,16 @@ def find_all_hidden_aps():
 	print "finding hidden ap's.."
 	sniff(iface=interface, prn=find_hidden_ap)
 
+def sniff_ap():
+	print "AP Sniffing.."
+	sniff(iface=interface, prn=find_hidden_ap)
+
 def create_ap():
+	print "Starting AP"
 	pkt = RadioTap() / Dot11(addr1="ff:ff:ff:ff:ff:ff", addr2='00:c0:ca:84:ab:a2', addr3='00:c0:ca:84:ab:a2') / Dot11Beacon(cap=0x1100) / Dot11Elt(ID=0, info="YO") / Dot11Elt(ID=1, info ="\x82\x84\x8b\x96\x24\x30\x48\x6c") / Dot11Elt(ID=3, info="\x0b") / Dot11Elt(ID=5, info="\x00\x01\x00\x00")
-	sendp(pkt, iface=interface, count = 10000, inter=.2)
+	sendp(pkt, iface=interface, count = 10000, inter=.1)
+	
+
 
 def setup_nic():
 	os.system('sudo ifconfig ' + interface + ' down')
@@ -83,7 +99,17 @@ def main():
 	elif sys.argv[1] == '-h':
 		find_all_hidden_aps()
 	elif sys.argv[1] == '-ap':
-		create_ap()
+		
+		t1 = threading.Thread( target=create_ap, args=() )
+		t1.setDaemon = True
+		t1.name = "Thread_1"
+		t1.start()
+
+		t2 = threading.Thread( target=sniff_ap, args=() )
+		t2.setDaemon = True
+		t2.name = "Thread_2"
+		t2.start()
+		
 
 if __name__ == "__main__":
     main()
